9bfb5dff24873baa34cdbf6546ea264b63d772d4
leonardo <leonardolfelix12@gmail.com>
2024-05-21 17:45:49 -0300

    feat(router): add resource de usuario

---
+++ backend/src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { PrismaService } from '@/prisma.service';
+
+@Module({
+  controllers: [UserController],
+  providers: [PrismaService, UserService],
+})
+export class UserModule {}
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import type { CreateUserDto } from './dto/create-user.dto';
+import type { PrismaService } from '@/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.usuario.create({
+      data: createUserDto,
+      select: {
+        id: true,
+        nome: true,
+      },
+    });
+
+    return user;
+  }
+
+  async findOne(id: number) {
+    const user = await this.prisma.usuario.findUniqueOrThrow({
+      select: {
+        id: true,
+        nome: true,
+        avatar: true,
+        jogador: {
+          select: {
+            id: true,
+          },
+        },
+      },
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+}
