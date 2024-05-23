4eeb2646ae511484fec56bd2a8dde7d877f9ab9a
leonardo <leonardolfelix12@gmail.com>
2024-05-22 21:14:09 -0300

    feat(module): criado modulo auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
