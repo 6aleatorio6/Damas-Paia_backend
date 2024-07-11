c48cea97902b585759faca21119d74943036f74d
leonardo <leonardolfelix12@gmail.com>
2024-07-11 00:09:56 -0300

    refactor(user): respostas dos endpoints alteradas

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,58 +5,42 @@ import {
   Body,
   Delete,
   Put,
-  Request,
-  BadRequestException,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Public } from '../auth/guard.service';
-import { hash } from 'bcrypt';
-
-const HASH_SALT = +process.env.HASH_SALT;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { IToken, Public, ReqUser } from 'src/auth/custom.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Public()
-  @Post()
-  async create(@Body() { nome, senha }: CreateUserDto) {
+  @Get('')
+  async findOneByToken(@ReqUser() ReqUser: IToken) {
     return {
-      message: 'usuario criado!',
-      data: await this.userService.create({
-        nome,
-        senha: await hash(senha, HASH_SALT),
-      }),
+      message: 'usuario encontrado!',
+      data: await this.userService.findOne(ReqUser.uuid),
     };
   }
 
-  @Get('')
-  async findOneByToken(@Request() req) {
-    return {
-      message: 'usuario encontrado!',
-      data: await this.userService.findOne(req.user.uuid),
-    };
+  @Public()
+  @Post()
+  async create(@Body() dtoUser: CreateUserDto) {
+    await this.userService.create(dtoUser);
   }
 
   @Put('')
-  async updateByToken(@Request() req, @Body() userDto: UpdateUserDto) {
-    if (Object.keys(userDto).length === 0)
-      throw new BadRequestException('Nenhum dado para atualizar');
-
-    const { senha, ...data } = userDto;
-
-    const senhaHash = senha && (await hash(senha, HASH_SALT));
-
-    await this.userService.update(req.user.uuid, { ...data, senha: senhaHash });
-    return { message: 'usuario atualizado!' };
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async updateByToken(@ReqUser() ReqUser, @Body() userDto: UpdateUserDto) {
+    await this.userService.update(ReqUser, userDto);
   }
 
   @Delete('')
-  async removeByToken(@Request() req) {
-    await this.userService.remove(req.user.uuid);
-
-    return { message: 'usuario deletado!' };
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async removeByToken(@ReqUser() ReqUser) {
+    await this.userService.remove(ReqUser.uuid);
   }
 }
