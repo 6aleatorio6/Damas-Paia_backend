d172d1e0fe6c76e33bdc46c844d3d16451c78ca5
leonardo <leonardolfelix12@gmail.com>
2024-07-11 23:08:44 -0300

    refactor(user, auth): AuthModule importando UserModule ao inves do typeOrm diretamente

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
 import { AuthGuard } from './auth.guard';
 import { ConfigService } from '@nestjs/config';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    UserModule,
     JwtModule.registerAsync({
       global: true,
       useFactory: (config: ConfigService) => ({
