8798cd18c0a68229e461aa6bb69b2219d7508bc6
leonardo <leonardolfelix12@gmail.com>
2024-07-12 00:44:49 -0300

    test(user): corrigido modo de testar rejects

--- src/user/tests/user.service.spec.ts
+++ src/user/tests/user.service.spec.ts
@@ -5,6 +5,7 @@ import { TestBed } from '@automock/jest';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { BadRequestException } from '@nestjs/common';
 
 const uuidUser = 'uuid-paia-paia-paia-paia';
 const oneUser: CreateUserDto = {
@@ -27,21 +28,19 @@ describe('UserService', () => {
 
   afterEach(jest.clearAllMocks);
 
-  it('método que verifica se o email ou nome já foram usados', () => {
+  it('método que verifica se o email ou nome já foram usados', async () => {
     db.findOne
       // o nome e email já existe no db
       .mockResolvedValueOnce({ password: undefined, ...oneUser } as User)
       // o email já existe no db
       .mockResolvedValueOnce({ email: oneUser.email } as User);
 
-    expect(userService.create(oneUser)).rejects.toHaveProperty(
-      'message',
-      'Esse email e nome já foram usado',
+    await expect(userService.create(oneUser)).rejects.toThrow(
+      new BadRequestException('Esse email e nome já foram usado'),
     );
 
-    expect(userService.create(oneUser)).rejects.toHaveProperty(
-      'message',
-      'Esse email já foi usado',
+    await expect(userService.create(oneUser)).rejects.toThrow(
+      new BadRequestException('Esse email já foi usado'),
     );
   });
 
@@ -57,9 +56,11 @@ describe('UserService', () => {
   });
 
   describe('Update', () => {
-    it('BadRequest se o dto vier vazio', () => {
+    it('BadRequest se o dto vier vazio', async () => {
       const updatePromise = userService.update(uuidUser, {});
-      expect(updatePromise).rejects.toHaveProperty('status', 400);
+      await expect(updatePromise).rejects.toThrow(
+        new BadRequestException('Nenhum dado foi enviado para atualização'),
+      );
     });
 
     it('atualizando um campo', async () => {
@@ -73,16 +74,19 @@ describe('UserService', () => {
   });
 
   describe('FindOne', () => {
-    it('BadRequest se não encontrar o user', () => {
+    it('BadRequest se não encontrar o user', async () => {
       const findOnePromise = userService.findOne(uuidUser);
-      expect(findOnePromise).rejects.toHaveProperty('status', 404);
+
+      await expect(findOnePromise).rejects.toThrow(
+        new BadRequestException('Usuário não encontrado'),
+      );
     });
 
-    it('Buscando um user que existe', () => {
+    it('Buscando um user que existe', async () => {
       db.findOne.mockResolvedValue(oneUser as any);
 
       const findOnePromise = userService.findOne(uuidUser);
-      expect(findOnePromise).resolves.toEqual(oneUser);
+      await expect(findOnePromise).resolves.toEqual(oneUser);
     });
   });
 });
