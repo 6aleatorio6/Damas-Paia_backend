49bd556b89af86927d04e4ea52a9828268fa68c6
leonardo <leonardolfelix12@gmail.com>
2024-07-12 21:16:41 -0300

    test(auth): feito testes para o service RefreshToken

--- src/auth/test/auth.service.spec.ts
+++ src/auth/test/auth.service.spec.ts
@@ -5,13 +5,20 @@ import { TestBed } from '@automock/jest';
 import { AuthService } from '../auth.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 jest.spyOn(bcrypt, 'compare').mockImplementation((s, d) => d === s);
 
 const username = 'leoPaia';
 const password = '123Leo';
 const uuid = 'uuid-paia-paia-paia-paia';
+
+const throwRefreshError = () => {
+  throw new UnauthorizedException('Atualize o token!', {
+    // o refreshToken verifica se o erro do getPayload tem essa descrição para renovar o token
+    description: 'refresh-token',
+  });
+};
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -53,6 +60,55 @@ describe('AuthService', () => {
       await expect(LoginPromise).rejects.toThrow(
         new BadRequestException('Usuário não encontrado'),
       );
+    });
+  });
+
+  describe('RefreshToken', () => {
+    it('sucesso no refresh', async () => {
+      guardMock.getPayloadJwt.mockImplementation(throwRefreshError);
+      jwtMock.decode.mockReturnValue({ uuid: 'uuid' });
+      jwtMock.signAsync.mockImplementation(async (x: any) => x.uuid);
+      dbMock.existsBy.mockResolvedValue(true);
+
+      // chamando o método para testar
+      const novoToken = await authService.refreshToken('paiaToken');
+
+      expect(jwtMock.decode).toHaveBeenLastCalledWith('paiaToken');
+      expect(novoToken).toBe('uuid');
+    });
+
+    it('user do token nao encontrada', async () => {
+      guardMock.getPayloadJwt.mockImplementation(throwRefreshError);
+      jwtMock.decode.mockReturnValue({ uuid: 'uuid' });
+      jwtMock.signAsync.mockImplementation(async (x: any) => x.uuid);
+      dbMock.existsBy.mockResolvedValue(false);
+
+      // chamando o método para testar
+      const refreshPromise = authService.refreshToken('paiaToken');
+      await expect(refreshPromise).rejects.toThrow(
+        new UnauthorizedException(
+          'Sua conta não foi encontrada, faça login novamente',
+        ),
+      );
+    });
+
+    it('O token fornecido não está expirado', async () => {
+      guardMock.getPayloadJwt.mockReturnValue({ uuid });
+
+      const refreshPromise = authService.refreshToken('paiaToken');
+      await expect(refreshPromise).rejects.toThrow(
+        new BadRequestException('O token ainda é valido'),
+      );
+    });
+
+    it('erro no token fora do escopo do refresh', async () => {
+      guardMock.getPayloadJwt.mockImplementation(() => {
+        throw new Error('paia error');
+      });
+
+      // chamando o método para testar
+      const refreshPromise = authService.refreshToken('paiaToken');
+      await expect(refreshPromise).rejects.toThrow(new Error('paia error'));
     });
   });
 });
