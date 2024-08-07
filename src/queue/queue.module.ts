fd645dbd467e1012b3e4ab49f987dab34a18dea2
leonardo <leonardolfelix12@gmail.com>
2024-08-07 19:50:59 -0300

    feat(queue):criando lógica de criar partida e peças

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { QueueGateway } from './queue.gateway';
+import { MatchModule } from 'src/match/match.module';
 
 @Module({
   providers: [QueueGateway, QueueService],
+  imports: [MatchModule],
 })
 export class QueueModule {}
--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,26 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import { CreateQueueDto } from './dto/create-queue.dto';
-import { UpdateQueueDto } from './dto/update-queue.dto';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource, EntityManager } from 'typeorm';
+import { UUID } from 'crypto';
+import { Match, Players } from 'src/match/entities/match.entity';
+import { Piece } from 'src/match/entities/piece.entity';
 
 @Injectable()
 export class QueueService {
-  create(createQueueDto: CreateQueueDto) {
-    return 'This action adds a new queue';
+  constructor(
+    @InjectDataSource()
+    private dataSource: DataSource,
+  ) {}
+
+  createMatch(createMatchDto: [UUID, UUID]) {
+    this.dataSource.transaction(async (manager) => {
+      let match = manager.create(Match, {
+        player1: { uuid: createMatchDto[0] },
+        player2: { uuid: createMatchDto[1] },
+      });
+
+      const pieces1 = this.createPiece(manager, match, 'player1');
+      const pieces2 = this.createPiece(manager, match, 'player2');
+      await manager.save([pieces1, pieces2]);
+
+      match = await manager.save(match);
+
+      return match.id;
+    });
   }
 
-  findAll() {
-    return `This action returns all queue`;
-  }
+  private createPiece(manager: EntityManager, match: Match, player: Players) {
+    const pY = player.endsWith('1') ? [0, 1] : [6, 7];
 
-  findOne(id: number) {
-    return `This action returns a #${id} queue`;
-  }
+    const pieces: Piece[] = [];
+    for (let i = 0; i < 8; i++) {
+      const piece = manager.create(Piece, {
+        match,
+        player: player,
+        x: i,
+        y: i % 2 === 0 ? pY[0] : pY[1],
+      });
 
-  update(id: number, updateQueueDto: UpdateQueueDto) {
-    return `This action updates a #${id} queue`;
-  }
+      pieces.push(piece);
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} queue`;
+    return pieces;
   }
 }
