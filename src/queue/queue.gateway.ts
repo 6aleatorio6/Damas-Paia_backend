4d33958563a2e938ee7d4de15e1ec383b955cb34
leonardo <leonardolfelix12@gmail.com>
2024-08-09 01:07:04 -0300

    feat(queue): implementando lógica de pareamento de usuários na fila

--- src/queue/queue.gateway.ts
+++ src/queue/queue.gateway.ts
@@ -1,34 +1,43 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 import { QueueService } from './queue.service';
-import { CreateQueueDto } from './dto/create-queue.dto';
-import { UpdateQueueDto } from './dto/update-queue.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UseFilters, UseGuards } from '@nestjs/common';
+import { WsExceptionsFilter } from 'src/wsException.filter';
+import { UUID } from 'crypto';
 
-@WebSocketGateway()
+@UseGuards(AuthGuard)
+@UseFilters(new WsExceptionsFilter())
+@WebSocketGateway({ namespace: 'queue', cors: true })
 export class QueueGateway {
+  @WebSocketServer() io: Server;
   constructor(private readonly queueService: QueueService) {}
 
-  @SubscribeMessage('createQueue')
-  create(@MessageBody() createQueueDto: CreateQueueDto) {
-    return this.queueService.create(createQueueDto);
-  }
+  /**
+   * 1 - Quando o usuario emit esse evento o Guard de autenticação autentica e add o obj user com o uuid no socket
+   * 2 - assim o usuario passa do filter e fica elegivel para o pareamento.
+   * 3 - Se tiver 2 usuarios no queue, cria um match e envia o uuid do match por um evento.
+   * 4 - Desconecta os usuarios do queue.
+   */
+  @SubscribeMessage('joinQueue')
+  async joinQueue() {
+    let sockets = await this.io.fetchSockets();
+    sockets = sockets.filter(({ data }) => data?.user);
 
-  @SubscribeMessage('findAllQueue')
-  findAll() {
-    return this.queueService.findAll();
-  }
+    if (sockets.length >= 2) {
+      const users = sockets.slice(0, 2);
+      const uuids = users.map(({ data }) => data.user.uuid) as [UUID, UUID];
 
-  @SubscribeMessage('findOneQueue')
-  findOne(@MessageBody() id: number) {
-    return this.queueService.findOne(id);
-  }
+      const matchUUID = await this.queueService.createMatch(uuids);
 
-  @SubscribeMessage('updateQueue')
-  update(@MessageBody() updateQueueDto: UpdateQueueDto) {
-    return this.queueService.update(updateQueueDto.id, updateQueueDto);
-  }
-
-  @SubscribeMessage('removeQueue')
-  remove(@MessageBody() id: number) {
-    return this.queueService.remove(id);
+      users.forEach((socket) => {
+        socket.emit('match', matchUUID);
+        socket.disconnect();
+      });
+    }
   }
 }
--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { QueueService } from './queue.service';
 import { QueueGateway } from './queue.gateway';
 import { MatchModule } from 'src/match/match.module';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Module({
-  providers: [QueueGateway, QueueService],
+  providers: [QueueGateway, QueueService, AuthGuard],
   imports: [MatchModule],
 })
 export class QueueModule {}
