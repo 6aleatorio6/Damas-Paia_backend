9590f04d165e0afd1b090748851804f3deca141b
leonardo <leonardolfelix12@gmail.com>
2024-08-10 02:09:47 -0300

    feat(main): enable cors and env port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  app.enableCors();
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
