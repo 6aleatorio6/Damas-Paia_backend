8f9657825d2c491a3da01be32224383ea8dc1f72
leonardo <leonardolfelix12@gmail.com>
2024-08-13 22:08:41 -0300

    test(e2e): movendo arquivo de testes  wsAdapter e renomeando reqMatch

---
+++ test/match/queue.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { createClient, wsTestAll } from 'test/wsHelper';
+
+describe('queue (Ws)', () => {
+  wsTestAll();
+
+  test('Entrando na fila e criando uma partida', async () => {
+    const client1 = await createClient();
+    const client2 = await createClient();
+
+    client1.emit('queue-match');
+    client2.emit('queue-match');
+
+    const [res, res2] = await Promise.all([
+      client2.onPaia('match'),
+      client2.onPaia('match'),
+    ]);
+
+    expect(res).toEqual(res2);
+    expect(res).toHaveLength(24);
+    expect(res[0]).toHaveProperty('x', 0);
+    expect(res[0]).toHaveProperty('y', 0);
+
+    expect(res.at(-1)).toHaveProperty('x', 7);
+    expect(res.at(-1)).toHaveProperty('y', 7);
+  });
+
+  test.only('Jogador se reconectando', async () => {
+    const client1 = await createClient();
+    const client2 = await createClient();
+
+    client1.emit('queue-match');
+    client2.emit('queue-match');
+
+    await Promise.all([client2.onPaia('match'), client2.onPaia('match')]);
+
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaa');
+    client2.disconnect();
+    client2.connect();
+
+    const [res, res2] = await Promise.all([
+      client2.onPaia('match'),
+      client2.onPaia('match'),
+    ]);
+
+    console.log(res, res2);
+  });
+});
---
+++ test/match/wsAdapter.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { createClient, wsTestAll } from 'test/wsHelper';
+
+describe('IoAdapterAuth (ws) ', () => {
+  wsTestAll();
+
+  test('conectando com token valido', async () => {
+    const client = await createClient();
+
+    return client.onPaia('connect');
+  });
+
+  test('conectando sem token', async () => {
+    const client = await createClient(null);
+
+    const error = await client.onPaia('connect_error', (e) =>
+      JSON.parse(e.context.responseText),
+    );
+
+    expect(error.message).toBe('INVALID');
+  });
+
+  test('conectando com token invalido', async () => {
+    const client = await createClient('Bearer tokenInvalido');
+
+    const error = await client.onPaia('connect_error', (e) =>
+      JSON.parse(e.context.responseText),
+    );
+
+    expect(error.message).toBe('INVALID');
+  });
+
+  test('conectando com token invalido', async () => {
+    const client = await createClient('Bearer tokenInvalido');
+
+    const error = await client.onPaia('connect_error', (e) =>
+      JSON.parse(e.context.responseText),
+    );
+
+    expect(error.message).toBe('INVALID');
+  });
+});
