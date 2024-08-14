88e615320ec4581a3cba641f66be8e179637a0d1
leonardo <leonardolfelix12@gmail.com>
2024-08-14 15:37:54 -0300

    refactor(match): Remove unused match-related files and tests

--- src/match/dto/update-match.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMatchDto } from './create-match.dto';
-
-export class UpdateMatchDto extends PartialType(CreateMatchDto) {
-  id: number;
-}
