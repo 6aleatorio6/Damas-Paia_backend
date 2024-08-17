8778dd38e9331ce118c6234f971a607464fc7fba
leonardo <leonardolfelix12@gmail.com>
2024-08-17 00:00:07 -0300

    feat(matchService): substituir array de direções por Enum; adicionar comentários; melhorar código

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -6,6 +6,22 @@ import { Piece } from './entities/piece.entity';
 import { MatchInfo } from './match';
 import { UUID } from 'crypto';
 
+// O TABULEIRO COMEÇA NO CANTO INFERIOR ESQUERDO
+// 7 [0,1,2,3,4,5,6,7]
+// ...
+// 0 [0,1,2,3,4,5,6,7]
+
+/**
+ *  [x, y]
+ */
+const dEnum = {
+  upRight: [-1, 1],
+  upLeft: [1, 1],
+  downRight: [-1, -1],
+  downLeft: [1, -1],
+} as const;
+type DMap = keyof typeof dEnum;
+
 @Injectable()
 export class MatchService {
   constructor(
@@ -17,33 +33,40 @@ export class MatchService {
 
   // async move(piece: Piece, move: PieceMove) {}
 
+  /**
+   * retorna um array de coordenadas possíveis para a peça se mover
+   */
   getMoviments({ piece, pieces }: PieceVerify) {
     const caminhos: Coord[][] = [];
-    const direcao: Direcao[] = [
-      [-1, 1], // cima esquerda
-      [1, 1], // cima direita
-      [-1, -1], // baixo esquerda
-      [1, -1], // baixo direita
-    ];
 
+    const mapMoviments = Object.keys(dEnum) as DMap[];
     // inverte a direção se for a vez do player2
-    if (piece.match.player2 == piece.player) direcao.reverse();
+    if (piece.match.player2 == piece.player) mapMoviments.reverse();
 
-    direcao.forEach((dir, i) => {
+    mapMoviments.forEach((dir, i) => {
       if (!piece.queen && i > 1) return;
-      const caminho = this.verifyCaminho(pieces, piece, dir);
-      caminhos.push(caminho.filter((c) => !c.piece).map((c) => c.coord));
+
+      let caminho = this.getPath(pieces, piece, dir);
+      caminho = caminho.filter((c) => !c.piece); // remove as casas com peças do caminho
+      caminhos.push(caminho.map((c) => c.coord)); // pega só as coordenadas
     });
 
-    return caminhos;
+    return caminhos.flat(); // achatando o array
   }
 
-  private verifyCaminho(pieces: Piece[], piece: Piece, direcao: Direcao) {
+  /**
+   * ele verifica o caminho da peça na direção passada e retorna um array de casas,
+   * sejam elas vazias ou ocupadas por peças adversárias. O caminho é interrompido
+   * na ultima casa disponível para se mover.
+   *
+   * um dia eu volto pra refatorar isso
+   */
+  getPath(pieces: Piece[], piece: Piece, direcao: DMap) {
     const caminho: Square[] = [];
 
     for (let i = 1; i <= 7; i++) {
-      const x = piece.x + direcao[0] * i;
-      const y = piece.y + direcao[1] * i;
+      const x = piece.x + dEnum[direcao][0] * i;
+      const y = piece.y + dEnum[direcao][1] * i;
 
       if (x < 0 || x > 7 || y < 0 || y > 7) break;
 
@@ -65,7 +88,11 @@ export class MatchService {
     return caminho;
   }
 
-  pieceVerify(matchInfo: MatchInfo, pieceId: number, userId: UUID) {
+  /**
+   * verifica se a peça é do jogador e se é a vez dele, se sim,
+   * retorna a peça e as peças do tabuleiro
+   */
+  verifyPiece(matchInfo: MatchInfo, pieceId: number, userId: UUID) {
     const isTurn = matchInfo.match.turn.uuid === userId;
     if (!isTurn) throw new BadRequestException('Não é seu turno');
 
@@ -81,5 +108,4 @@ export class MatchService {
 
 export type Coord = { x: number; y: number };
 type PieceVerify = { piece: Piece; pieces: Piece[] };
-type Direcao = [number, number];
 type Square = { coord: Coord; piece?: Piece };
