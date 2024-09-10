e3792c331fad78600dd6c2e30915f64d2fa0b238
leonardo <leonardolfelix12@gmail.com>
2024-09-10 00:51:05 -0300

    test(e2e/match): adicionar teste para verificar comportamento ao sair da fila

--- test/match/match.e2e-spec.ts
+++ test/match/match.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('queue (Ws)', () => {
     const client1 = await createClient();
     const client2 = await createClient();
 
-    client1.emit('match:queue');
-    client2.emit('match:queue');
+    client1.emit('match:queue', 'join');
+    client2.emit('match:queue', 'join');
 
     const [res, res2] = await Promise.all([
       client1.onPaia('match:start'),
@@ -22,4 +22,34 @@ describe('queue (Ws)', () => {
     expect(res.myPlayer.pieces).toHaveLength(12);
     expect(res.playerOponent.pieces).toHaveLength(12);
   });
+
+  test('Deve retornar Badrequest ao passar uma action errada no Match:queue', async () => {
+    const client = await createClient();
+
+    client.emit('match:queue', 'joinPaia');
+
+    const res = await client.onPaia('error');
+
+    expect(res).toMatchObject({
+      error: 'Bad Request',
+      message: 'Validation failed (enum string is expected)',
+      statusCode: 400,
+    });
+  });
+
+  test.failing(
+    'Não deve iniciar uma partida, já que o client1 saiu da fila',
+    async () => {
+      const client1 = await createClient();
+      const client2 = await createClient();
+
+      client1.emit('match:queue', 'join');
+      client1.emit('match:queue', 'leave');
+      client2.emit('match:queue', 'join');
+
+      const res = await client1.onPaia('match:start');
+      expect(res).toBeDefined();
+    },
+    200,
+  );
 });
