ffea65955d0505f26489b3bee15658f90eb4f538
leonardo <leonardolfelix12@gmail.com>
2024-09-26 00:16:10 -0300

    test(e2e/match): dividir testes do `match-reconnect` em 2 arquivos
    - Os testes de finalização de partidas e abandono de jogadores foram movidos do arquivo `match-reconnect.e2e-spec.ts` para um novo arquivo `match-finalizer.e2e-spec.ts`.
    - O arquivo `match-reconnect.e2e-spec.ts` agora se concentra apenas nos cenários de desconexão e reconexão, mantendo a organização e clareza nos testes.

--- test/match/match-reconnect.e2e-spec.ts
+++ test/match/match-reconnect.e2e-spec.ts
@@ -5,65 +5,6 @@ import { testApp } from 'test/setup';
 import { createClient, createMatch } from 'test/wsHelper';
 
 describe('match (Ws)', () => {
-  describe('Cenario de fim de jogo', () => {
-    test('Deve finalizar a partida e desconectar os jogadores', async () => {
-      const { client1, client2, matC2 } = await createMatch();
-      const [match, pieces] = matC2;
-
-      // apagar todas as peças do jogador 2
-      const pieceRepo = testApp.get(getRepositoryToken(Piece));
-      await pieceRepo.delete({ match: { uuid: match.uuid }, player: 'player2' });
-
-      // mover a peça do jogador 1 para dar checkmate
-      const piece = pieces.find((p) => p.player === 'player1' && p.x === 1 && p.y === 2);
-      client1.emit('match:paths', piece.id);
-      client1.emit('match:move', { id: piece.id, to: { x: 2, y: 3 } });
-
-      const [res] = await client1.onPaia('match:finish');
-      expect(res).toHaveProperty('winner', 'player1');
-      expect(res).toHaveProperty('winnerStatus', 'checkmate');
-      expect(res).toHaveProperty('dateEnd');
-      expect(client1.disconnected).toBe(true);
-    });
-  });
-
-  describe('Cenários de Abandono de Partida (match:quit)', () => {
-    test('Jogador abandona uma partida em andamento', async () => {
-      const pieceRepo = testApp.get(getRepositoryToken(Piece));
-      const { client1, client2, matC2 } = await createMatch();
-      const [match] = matC2;
-
-      client1.emit('match:quit');
-
-      const [[res1], [res2]] = await Promise.all([
-        client1.onPaia('match:finish'),
-        client2.onPaia('match:finish'),
-      ]);
-
-      expect(res1).toEqual(res2);
-      expect(res1.winner).toBe('player2');
-      expect(res1).toHaveProperty('dateEnd');
-      expect(res1).toHaveProperty('winnerStatus', 'resign');
-
-      const pieceQuery = pieceRepo.findBy({ match: { uuid: match.uuid } });
-      await expect(pieceQuery).resolves.toHaveLength(0);
-      expect(client1.disconnected).toBe(true);
-    });
-
-    test('Deve retornar erro 400 ao tentar sair de uma partida inexistente', async () => {
-      const client = await createClient();
-
-      client.emit('match:quit');
-
-      const [res] = await client.onPaia('error');
-      expect(res).toMatchObject({
-        error: 'Bad Request',
-        message: 'Partida não encontrada',
-        statusCode: 400,
-      });
-    });
-  });
-
   describe('Cenários de Desconexão e Reconexão', () => {
     test('Deve declarar o jogador 2 como vencedor se o jogador 1 não se reconectar', async () => {
       testApp.get(ConfigService).set('TIMEOUT_TO_RECONNECT', 100);
