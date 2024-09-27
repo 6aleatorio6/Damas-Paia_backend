039c3e7ef6aa89c2e31f43c23fdd249d2b15ceba
leonardo <leonardolfelix12@gmail.com>
2024-09-27 16:47:50 -0300

    refactor(oauth2): implementar endpoint de callback
    - Removidos métodos não utilizados (create, findAll, findOne, update, remove)
    - Implementado endpoint GET para callback que processa o código de autenticação do provedor
    - O método `callback` agora chama `signOrLogin` com os parâmetros providerName e authCode, retornando o token

--- src/oauth2/oauth2.controller.ts
+++ src/oauth2/oauth2.controller.ts
@@ -1,34 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Query, Redirect } from '@nestjs/common';
 import { Oauth2Service } from './oauth2.service';
-import { CreateOauth2Dto } from './dto/create-oauth2.dto';
-import { UpdateOauth2Dto } from './dto/update-oauth2.dto';
+import { Public } from 'src/auth/custom.decorator';
 
 @Controller('oauth2')
 export class Oauth2Controller {
   constructor(private readonly oauth2Service: Oauth2Service) {}
 
-  @Post()
-  create(@Body() createOauth2Dto: CreateOauth2Dto) {
-    return this.oauth2Service.create(createOauth2Dto);
-  }
-
-  @Get()
-  findAll() {
-    return this.oauth2Service.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.oauth2Service.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOauth2Dto: UpdateOauth2Dto) {
-    return this.oauth2Service.update(+id, updateOauth2Dto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.oauth2Service.remove(+id);
+  @Public()
+  @Get(':providerName/:authCode')
+  async callback(
+    @Param('providerName') providerName: string,
+    @Param('authCode') authCode: string,
+  ) {
+    return { token: await this.oauth2Service.signOrLogin(providerName, authCode) };
   }
 }
